a1d553b563c9bcf3db3bc5b7e90ad0c18679dd27
Deirdre O'Halloran <Deirdre.OHalloran@setu.ie>
2026-02-04 09:43:14 +0000

    changes now being deployed to Github and Render

--- controllers/start.js
+++ controllers/start.js
@@ -8,7 +8,7 @@ const start = {
     logger.info("Start page loading!");
     
     const viewData = {
-      title: "Welcome to the Playlist app!",
+      title: "Welcome to Deirdres Playlist app!",
       info: appStore.getAppInfo()
     };
     
