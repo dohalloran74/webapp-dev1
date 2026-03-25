bdfeb242d68268829d7c4798d3c0dbf5f49d93eb
Deirdre O'Halloran <Deirdre.OHalloran@setu.ie>
2026-03-25 10:20:05 +0000

    Playlist app stats

--- controllers/stats.js
+++ controllers/stats.js
@@ -12,14 +12,43 @@ const stats = {
     
     let numSongs = playlists.reduce((total, playlist) => total + playlist.songs.length, 0);
 	
-	let average =  numPlaylists > 0 ? numSongs/numPlaylists : 0;
+	  let average = numPlaylists > 0 ? (numSongs / numPlaylists).toFixed(2) : 0;
 
+    const ratings = playlists
+  .map((playlist) => Number(playlist.rating))
+  .filter((rating) => !Number.isNaN(rating));
 
-    const statistics = {
-      displayNumPlaylists: numPlaylists,
-      displayNumSongs: numSongs,
-	  displayAverage: average.toFixed(2)
-    }
+let totalRating = ratings.reduce((total, rating) => total + rating, 0);
+
+let avgRating = ratings.length > 0 ? totalRating / ratings.length : 0;
+
+let maxRating = ratings.length > 0 ? Math.max(...ratings) : 0;
+
+let maxRated = playlists.filter(
+  (playlist) => Number(playlist.rating) === maxRating
+);
+
+let favTitles = maxRated.map((item) => item.title);
+
+    /*
+
+    let totalRating = playlists.reduce((total, playlist) => total + Number(playlist.rating), 0);
+
+    let avgRating = numPlaylists > 0 ? totalRating/numPlaylists : 0;
+
+let maxRating = Math.max(...playlists.map(playlist => playlist.rating));
+let maxRated = playlists.filter(playlist =>Number(playlist.rating) === maxRating);
+let favTitles = maxRated.map(item => item.title);
+*/
+const statistics = {
+    displayNumPlaylists: numPlaylists,
+    displayNumSongs: numSongs,
+    displayAverage: average,
+    displayAvgRating: avgRating.toFixed(2),
+	  highest: maxRating,
+    displayFav: favTitles
+};
+
 
     const viewData = {
       title: "Playlist App Statistics",
